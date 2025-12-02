04fdd1fb7e10217d25fe4563283d3f540e2b2f93
Murad Salikhov <murad.27012@yandex.ru>
2025-12-02 22:42:00 +0700

    Возвращаю файлы

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+import { createComments } from './comments.js';
+import { createUrl, createDescription, createLikes } from './objectValues.js';
+import { MAX_POSTS_COUNT } from './data.js';
 
+const objectsArray = [];
+
+const createPosts = () => {
+  for (let i = 1; i <= MAX_POSTS_COUNT; i++) {
+    objectsArray.push({
+      id: i,
+      url: createUrl(i),
+      description: createDescription(),
+      likes: createLikes(),
+      comments: createComments()
+    });
+  }
+  return objectsArray;
+};
+
+createPosts();
