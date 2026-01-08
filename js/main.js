f1f0ba9808eef1835f4c89113b9577e623405c64
Murad Salikhov <murad.27012@yandex.ru>
2026-01-09 01:53:44 +0700

    Правки v2.0

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ import { renderPictures } from './pictures.js';
 import { initializePost } from './post-modal.js';
 import { initializeFilters } from './filters.js';
 
-initializePost();
 getPosts()
   .then((posts) => {
     initializeFilters(posts);
     renderPictures(posts);
   })
   .catch((error) => showAlert(error.message));
+initializePost();
